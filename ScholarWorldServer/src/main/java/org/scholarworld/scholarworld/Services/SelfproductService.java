4f412870f22a38803357c47e5a38aec487fcca68
rohan-vishnoi <avirohanvishnoi@gmail.com>
2025-03-14 00:29:03 +0530

    #OPS - 31 Product Controller Changes

--- ScholarWorldServer/src/main/java/org/scholarworld/scholarworld/Services/SelfproductService.java
+++ ScholarWorldServer/src/main/java/org/scholarworld/scholarworld/Services/SelfproductService.java
@@ -36,7 +36,7 @@ public class SelfproductService implements ProductService {
 
     @Override
     public List<Product> getAllProducts() {
-        return List.of();
+        return productRepo.getAllProducts();
     }
 
     @Override
--- ScholarWorldServer/src/main/java/org/scholarworld/scholarworld/repositories/ProductRepo.java
+++ ScholarWorldServer/src/main/java/org/scholarworld/scholarworld/repositories/ProductRepo.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -22,4 +23,7 @@ public interface ProductRepo extends JpaRepository<Product, Long> {
     //HQL
     @Query("select p.title as title, p.description as description from Product p where p.Id = :id")
     ProductWithTitleAndDescription someRandomQuery(@Param("id") Long id);
+
+    @Query("select p from Product p")
+    List<Product> getAllProducts();
 }
