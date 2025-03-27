40bf8207fcf7dbcb8b70ec48a6201f53ad46a950
rohan-vishnoi <avirohanvishnoi@gmail.com>
2025-03-27 06:57:06 +0530

    #OPS - 12 Email Service
    Controller , Class and Service Changes

---
+++ ScholarWorldServer/emailService/src/main/java/org/scholarworld/emailservice/dtos/SendEmailEventDto.java
@@ -0,0 +1,47 @@
+package org.scholarworld.emailservice.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SendEmailEventDto {
+    private String to;
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    private String from;
+    private String subject;
+    private String body;
+
+}
